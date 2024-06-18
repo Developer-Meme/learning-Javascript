c712b0ed65dabddfa36aee9445f654855a885ed9
Developer_Meme <victormax9154@gmail.com>
2024-06-18 23:37:31 +0300

    Refactor JavaScript code to use lowercase ID for myRuby element

--- Javascript/index.js
+++ Javascript/index.js
@@ -2,11 +2,11 @@
 //Date: 18th June 2021
 /*To output some text i can write
 */
-console.log(`Developer Meme`)
-console.log(`Like luffy i love meat`)
+//console.log(`Developer Meme`)
+//console.log(`Like luffy i love meat`)
 //To create an alert box u write
-window.alert(`Hello World`)
-window.alert(`Future Nextjs senior developer`)
+//window.alert(`Hello World`)
+//window.alert(`Future Nextjs senior developer`)
 //this is a single line comment
 /*this
 is a
@@ -16,9 +16,14 @@ comment
 /*To change the content of an Html element i have to select it first 
 then change the text content then set it equal to the text of your choosing
 */
-document.getElementById("myRuby").textContent=`Hey there!`;
-document.getElementById("myDove") .textContent=`Am Developer Meme`;
+//document.getElementById("myRuby").textContent=`Hey there!`;
+//document.getElementById("myDove") .textContent=`Am Developer Meme`;
 /*A VAriable is a container that stores a value.
 the value behaves as if it were the value it contains
 There are 2 ways of using variables 1]Declaration let x; 2]Assignment x-100
 */
+//one variables only has one value it cant be changed only one x or y
+let p;
+x = 100;
+
+console.log(`p`);
