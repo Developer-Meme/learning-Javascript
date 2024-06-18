f67ba98a3d74a063c40bb8bff03c3c9d59669022
Developer_Meme <victormax9154@gmail.com>
2024-06-18 22:59:15 +0300

    chore: Update cSpell dictionary with JavaScript and Next.js keywords

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,6 +1,15 @@
 //learning Javascript from scratch from bro code
+//Date: 18th June 2021
 /*To output some text i can write
-
 */
 console.log(`Developer Meme`)
 console.log(`Like luffy i love meat`)
+//To create an alert box u write
+window.alert(`Hello World`)
+window.alert(`Future Nextjs senior developer`)
+//this is a single line comment
+/*this
+is a
+multi-line
+comment
+*/  
